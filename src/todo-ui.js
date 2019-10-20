4fc5d010765d83f15843819f05994505642f0931
yy523697597 <523697597@qq.com>
2019-10-20 17:49:54 +0800

    重构ui组件为无状态组件

--- src/todo-ui.js
+++ src/todo-ui.js
@@ -3,34 +3,30 @@ import React, { Component, Fragment } from "react";
 import { Input, Button, List } from "antd";
 import "antd/dist/antd.css";
 
-class TodoListUi extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div>
-          <Input
-            value={this.props.inputValue}
-            onChange={this.props.changeInputValue}
-            placeholder="请输入您的服务"
-            style={{ width: "250px", margin: "10px 10px" }}
-          />
-          <Button type="primary" onClick={this.props.addList}>
-            添加日程
-          </Button>
-        </div>
-        <List
-          bordered
-          dataSource={this.props.list}
-          renderItem={(item, index) => (
-            <List.Item onClick={() => this.props.deleteItem(index)}>
-              {item}
-            </List.Item>
-          )}
-          style={{ width: "250px", marginLeft: "10px" }}
+const TodoListUi = props => {
+  return (
+    <Fragment>
+      <div>
+        <Input
+          value={props.inputValue}
+          onChange={props.changeInputValue}
+          placeholder="请输入您的服务"
+          style={{ width: "250px", margin: "10px 10px" }}
         />
-      </Fragment>
-    );
-  }
-}
+        <Button type="primary" onClick={props.addList}>
+          添加日程
+        </Button>
+      </div>
+      <List
+        bordered
+        dataSource={props.list}
+        renderItem={(item, index) => (
+          <List.Item onClick={() => props.deleteItem(index)}>{item}</List.Item>
+        )}
+        style={{ width: "250px", marginLeft: "10px" }}
+      />
+    </Fragment>
+  );
+};
 
 export default TodoListUi;
